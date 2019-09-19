18d6b5818523f88d468bf8ab8ebe70371122ac59
ArielLB <arielelbachar@gmail.com>
2019-09-19 11:28:32 +0300

    parse-url-return-dish-name

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 document.querySelector('#search-button').addEventListener("click", main);
 
 var pageNumber = 1;
+
+
+
+
 function main() {
-    //9afc229df852a2a227f0d3cf7ee8ee3d
-    var baseUrl = "https://www.food2fork.com/api/search?key=9afc229df852a2a227f0d3cf7ee8ee3d&q=chicken%20leg&page=1";
-
-    var dishName = document.getElementById('dish-input').value;
-    var ingredients = document.getElementById('ingredients-input').value;
+    var dishName = parseDishName(document.getElementById('dish-input').value);
+    var baseUrl = "https://www.food2fork.com/api/search?key=9afc229df852a2a227f0d3cf7ee8ee3d&q="
+        + dishName + "&page=" + pageNumber;
 
 
-
-    // baseUrl = baseUrl + dishName + "&p=" + pageNumber;
-    fetch(baseUrl)
-
-        .then(response => {
-            return response.json();
-        })
+    fetch(baseUrl).then(response => { return response.json(); })
         .then(function (response) {
-
             document.querySelector('#recipe-name-h1').innerHTML = response.recipes.map(function (item) {
-                console.log(item.title);
-                return ''
-                    + '<div>'
-                    + '<p>' + item.title + '</p>'
-                    + '</div>';
+                return item.title + '<p>';
             }).join("");
 
 
@@ -43,4 +34,8 @@ function main() {
 
 
 
+}
+
+function parseDishName(str) {
+    return str.split(" ").join("%20");
 }
